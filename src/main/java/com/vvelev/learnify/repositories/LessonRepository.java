5ceda599d7bb1f970f2fbaa157d22856b147caaf
Vasil Velev <vasilvelev07@gmail.com>
2025-11-23 03:50:20 +0200

    lesson repository created

---
+++ src/main/java/com/vvelev/learnify/repositories/LessonRepository.java
@@ -0,0 +1,9 @@
+package com.vvelev.learnify.repositories;
+
+import com.vvelev.learnify.entities.Lesson;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LessonRepository extends JpaRepository<Lesson, Long> {
+}
