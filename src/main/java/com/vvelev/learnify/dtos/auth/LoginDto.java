0068ee9111aaa372108c573fe7543f7c542f8e74
Vasil Velev <vasilvelev07@gmail.com>
2025-12-20 18:48:58 +0200

    added validations to auth dtos

--- src/main/java/com/vvelev/learnify/dtos/auth/LoginDto.java
+++ src/main/java/com/vvelev/learnify/dtos/auth/LoginDto.java
@@ -1,5 +1,8 @@
 package com.vvelev.learnify.dtos.auth;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,6 +13,11 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginDto {
+    @NotBlank(message = "Email is required")
+    @Email
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 50, message = "Password must be between 6 to 50 characters long")
     private String password;
 }
--- src/main/java/com/vvelev/learnify/dtos/auth/RegisterDto.java
+++ src/main/java/com/vvelev/learnify/dtos/auth/RegisterDto.java
@@ -1,5 +1,8 @@
 package com.vvelev.learnify.dtos.auth;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,9 +13,20 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterDto {
+    @NotBlank(message = "Email is required")
+    @Email
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 50, message = "Password must be between 6 to 50 characters long")
     private String password;
+
+    @NotBlank(message = "Confirm password is required")
     private String confirmPassword;
+
+    @NotBlank(message = "First name is required")
     private String firstName;
+
+    @NotBlank(message = "Last name is required")
     private String lastName;
 }
