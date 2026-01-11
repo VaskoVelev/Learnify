87ef58101c2e88c055f564945d10d1974325241c
Vasko1234 <vasetovelev07@gmail.com>
2026-01-11 20:41:36 +0200

    question dtos cleaned up

--- src/main/java/com/vvelev/learnify/dtos/question/UpdateQuestionDto.java
+++ src/main/java/com/vvelev/learnify/dtos/question/UpdateQuestionDto.java
@@ -4,18 +4,12 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
+@Getter @Setter
 @AllArgsConstructor
 public class UpdateQuestionDto {
     @NotBlank(message = "Text is required")
     @Size(max = 2000)
     private String text;
-
-    @NotBlank
-    private String type;
 }
