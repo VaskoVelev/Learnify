bb69407796e323c12351cca0ea5c5ab0d261ccb0
Vasko1234 <vasetovelev07@gmail.com>
2026-01-15 01:35:28 +0200

    created test files

---
+++ src/test/java/com/vvelev/learnify/services/QuestionServiceTest.java
@@ -0,0 +1,8 @@
+package com.vvelev.learnify.services;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class QuestionServiceTest {
+}
